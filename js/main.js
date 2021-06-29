f735aa892097ac2ccba3e8e66982cd8dd1a9fafc
GreenSnak <86664888+GreenSnak@users.noreply.github.com>
2021-06-29 16:02:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// mob menu
+
+const menuButton = document.querySelector('.menu-button');
+const menu = document.querySelector('.nav-menu');
+const menuCloseButton = document.querySelector('.menu-close');
+
+menuButton.addEventListener('click', () => {
+   // console.log('Вы кликнули на кнопку меню')
+   menu.classList.add('is-active');
+   menuCloseButton.classList.add('is-active');
+});
+
+menuCloseButton.addEventListener('click', () => {
+   // console.log('Вы кликнули на кнопку меню')
+   menu.classList.remove('is-active');
+   menuCloseButton.classList.remove('is-active');
+});
+
